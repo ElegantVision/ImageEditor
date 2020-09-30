8ee09e897dc187b88085b48c34fd9d8e49219c8c
Joshua <joshual0821@gmail.com>
2020-09-30 14:41:25 -0500

    Add project files.

---
+++ ImageResizer/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace ImageResizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtslct = new System.Windows.Forms.TextBox();
+            this.txtsv = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtw = new System.Windows.Forms.TextBox();
+            this.txth = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Resized = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ImageCompressor = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.ResizePanel = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Resized)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.ResizePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Image";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Save Image";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtslct
+            // 
+            this.txtslct.Location = new System.Drawing.Point(78, 27);
+            this.txtslct.Name = "txtslct";
+            this.txtslct.Size = new System.Drawing.Size(349, 20);
+            this.txtslct.TabIndex = 2;
+            // 
+            // txtsv
+            // 
+            this.txtsv.Location = new System.Drawing.Point(78, 88);
+            this.txtsv.Name = "txtsv";
+            this.txtsv.Size = new System.Drawing.Size(349, 20);
+            this.txtsv.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(433, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Width";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(353, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Type";
+            // 
+            // txtw
+            // 
+            this.txtw.Location = new System.Drawing.Point(76, 151);
+            this.txtw.Name = "txtw";
+            this.txtw.Size = new System.Drawing.Size(66, 20);
+            this.txtw.TabIndex = 9;
+            // 
+            // txth
+            // 
+            this.txth.Location = new System.Drawing.Point(232, 151);
+            this.txth.Name = "txth";
+            this.txth.Size = new System.Drawing.Size(66, 20);
+            this.txth.TabIndex = 10;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(390, 151);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(67, 21);
+            this.comboBox.TabIndex = 12;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(61, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 29);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Resize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(265, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 29);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(11, 245);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(294, 190);
+            this.pictureBox.TabIndex = 15;
+            this.pictureBox.TabStop = false;
+            // 
+            // pictureBox_Resized
+            // 
+            this.pictureBox_Resized.Location = new System.Drawing.Point(16, 44);
+            this.pictureBox_Resized.Name = "pictureBox_Resized";
+            this.pictureBox_Resized.Size = new System.Drawing.Size(428, 296);
+            this.pictureBox_Resized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox_Resized.TabIndex = 16;
+            this.pictureBox_Resized.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 4);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(538, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Resized : (left press mouse beneathe top border of this blue shaded area. and mov" +
+    "e mouse to pan around image)";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Original";
+            // 
+            // ImageCompressor
+            // 
+            this.ImageCompressor.Location = new System.Drawing.Point(510, 193);
+            this.ImageCompressor.Name = "ImageCompressor";
+            this.ImageCompressor.Size = new System.Drawing.Size(112, 23);
+            this.ImageCompressor.TabIndex = 19;
+            this.ImageCompressor.Text = "Compress";
+            this.ImageCompressor.UseVisualStyleBackColor = true;
+            this.ImageCompressor.Click += new System.EventHandler(this.ImageCompressor_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(675, 194);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(198, 45);
+            this.trackBar1.TabIndex = 20;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // ResizePanel
+            // 
+            this.ResizePanel.AutoScroll = true;
+            this.ResizePanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ResizePanel.Controls.Add(this.pictureBox_Resized);
+            this.ResizePanel.Controls.Add(this.label6);
+            this.ResizePanel.Location = new System.Drawing.Point(356, 225);
+            this.ResizePanel.Name = "ResizePanel";
+            this.ResizePanel.Size = new System.Drawing.Size(653, 441);
+            this.ResizePanel.TabIndex = 21;
+            this.ResizePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ResizePanel_MouseDown);
+            this.ResizePanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ResizePanel_MouseMove);
+            this.ResizePanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ResizePanel_MouseUp);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(570, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(290, 52);
+            this.label8.TabIndex = 18;
+            this.label8.Text = resources.GetString("label8.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 678);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.ResizePanel);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.ImageCompressor);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.txth);
+            this.Controls.Add(this.txtw);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsv);
+            this.Controls.Add(this.txtslct);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Image Rezier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Resized)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResizePanel.ResumeLayout(false);
+            this.ResizePanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtslct;
+        private System.Windows.Forms.TextBox txtsv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtw;
+        private System.Windows.Forms.TextBox txth;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox pictureBox_Resized;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button ImageCompressor;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel ResizePanel;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ ImageResizer/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageResizer
+{
+    public partial class Form1 : Form
+    {
+        //create new image
+        Image img;
+
+        //array of file extensions
+        string[] exten = { ".PNG", ".JPG", ".JPEG", ".GIF" };
+        Point mosloc;
+
+        PictureBox org;
+        bool pan = false;//when true can pan
+        
+
+        Image ZoomPicture(Image img2, Size size)
+        {
+            Bitmap bm = new Bitmap(img2, Convert.ToInt32(img.Width * size.Width), Convert.ToInt32(img.Height * size.Height));
+            Graphics gpu = Graphics.FromImage(bm);
+            gpu.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bm;
+        }
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < exten.Length; i++)
+            {
+                comboBox.Items.Add(exten[i]);
+            }
+
+
+            //assigning values and initializing variables (this is all done here just for testing stuff)
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 6;
+            trackBar1.SmallChange = 1;
+            trackBar1.LargeChange = 1;
+            trackBar1.UseWaitCursor = false;
+
+            this.DoubleBuffered = true;
+            org = new PictureBox();
+            mosloc = new Point();
+            ResizePanel.HorizontalScroll.Value = 10;
+            ResizePanel.HorizontalScroll.SmallChange = 1;
+            ResizePanel.VerticalScroll.SmallChange = 1;
+            //
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //getting image
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "images | *.png;*jpg;*jpeg;*gif";
+
+            //checking ofd info
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtslct.Text = ofd.FileName;
+                img = Image.FromFile(ofd.FileName);
+                pictureBox.Image = img;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Saving Image
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtsv.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //resizing image
+            int
+                w = Convert.ToInt32(txtw.Text),
+                h = Convert.ToInt32(txth.Text);
+
+            img = Resize(img, w, h);
+
+            ((Button)sender).Enabled = false;
+            
+            MessageBox.Show("image resized");
+
+        }
+
+        Image Resize(Image image, int w, int h)
+        {
+            
+            //creating new bitmap and graphic from that bitmap
+            Bitmap bmp = new Bitmap(w, h);
+            Graphics graphic = Graphics.FromImage(bmp);
+
+            //drawing image / disposing it
+            graphic.DrawImage(image, 0, 0, w, h);
+            graphic.Dispose();
+
+            pictureBox_Resized.Image = bmp;
+            //return the new bmp
+            return bmp;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Saving Image
+            int
+                dot = 0,
+                slash = 0;
+
+            //using a longer way to store index of a . and \ of the path of selected image
+            for (int j = txtslct.Text.Length-1; j >= 0; j--)
+            {
+                if (txtslct.Text[j] == '.')
+                {
+                    dot = j;
+                }
+                else if (txtslct.Text[j] == '\\')
+                {
+                    slash = j;
+                    break; //leaves for loop
+                }
+            }
+
+            //rebuilding the saving path and name and fileextension of edited image
+            img.Save(txtsv.Text + "\\" + txtslct.Text.Substring(slash + 1, dot - slash - 1) + exten[comboBox.SelectedIndex]);
+            ((Button)sender).Enabled = false;
+            MessageBox.Show("Image Saved");
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //When the Compressor button is pressed
+        private void ImageCompressor_Click(object sender, EventArgs e)
+        {
+            //this method handles the compressing process of the image
+            CompressImage(txtslct.Text, 50, txtsv.Text);
+        }
+
+        #region MethodsForCreatingCompression
+        //The Compressing Process / Proceedure for the image
+        private string CompressImage(string InputImage, int Quality, string OutPutDirectory)
+        {
+            using (Bitmap mybitmab = new Bitmap(@InputImage))
+            {
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+                System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, Quality);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                string NewOutPutPath = OutPutDirectory + "\\" + Path.GetFileNameWithoutExtension(InputImage) + Path.GetExtension(InputImage);
+
+                mybitmab.Save(NewOutPutPath, jpgEncoder, myEncoderParameters);
+
+                return NewOutPutPath;
+            }
+        }
+
+        //Gets the encoder that will be used for the image
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+        #endregion
+
+        //Allowing the trackbar to zoom the image
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (org.Image == null)
+            {
+                org.Image = pictureBox_Resized.Image;
+            }
+            
+            //creating the zooming behaviour
+            if (trackBar1.Value != 0)
+            {
+                pictureBox_Resized.Image = null;
+                pictureBox_Resized.Image = ZoomPicture(org.Image, new Size(trackBar1.Value, trackBar1.Value));
+            }
+        }
+
+        //Mouse Down/Up/Move all handles the process of allowing user to pan the image around in the picturebox
+        private void ResizePanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            pan = true;
+            mosloc = e.Location;
+        }
+
+        private void ResizePanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            pan = false;
+            mosloc = new Point();
+        }
+
+        private void ResizePanel_MouseMove(object sender, MouseEventArgs e)
+        {//set last position to new mosue position every second
+            if (pan == true)
+            {
+                if (e.Location.X < mosloc.X)
+                {
+                    if (ResizePanel.HorizontalScroll.Value <= ResizePanel.HorizontalScroll.Maximum-5)
+                    {
+
+                        if (ResizePanel.HorizontalScroll.Value < ResizePanel.HorizontalScroll.Maximum - 5)
+                        {
+                            ResizePanel.HorizontalScroll.Value += 5;
+                            mosloc = e.Location;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+                else if (e.Location.X > mosloc.X)
+                {
+                    if (ResizePanel.HorizontalScroll.Value >= 0+5)
+                    {
+                        
+                        if (ResizePanel.HorizontalScroll.Value <= 0+5)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            ResizePanel.HorizontalScroll.Value -= 5;
+                            mosloc = e.Location;
+                        }
+                    }
+                    
+                }
+
+            }
+        }
+    }
+}
